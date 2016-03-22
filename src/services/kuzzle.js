8d4699ac44dd60f08972a93a4ee411be0634c6e3
Anthony Sendra <asendra@kaliop.com>
2016-03-22 16:50:42 +0100

    Step 1 : Initialize a Kuzzle object

--- src/services/kuzzle.js
+++ src/services/kuzzle.js
@@ -2,6 +2,6 @@ import Kuzzle from 'kuzzle-sdk'
 import Config from '../config'
 
 /** TODO - Step 1: initialize a Kuzzle object */
-var kuzzle = {};
+var kuzzle = new Kuzzle(Config.kuzzleUrl, {defaultIndex: Config.defaultIndex});
 
 export default kuzzle;
